bd4f674a613daa0dc1d0986e345d525beffecf67
Indranil Sinha <indzi@programmer.net>
2018-05-26 15:40:10 +0430

    Accounts DAO interface and imp created

--- src/main/java/com/athena/accounts/dao/AccountsDAO.java
+++ src/main/java/com/athena/accounts/dao/AccountsDAO.java
@@ -1,4 +1,15 @@
 package com.athena.accounts.dao;
 
-public class AccountsDAO {
+import com.athena.accounts.model.AccountsModel;
+
+import java.util.List;
+
+public interface AccountsDAO {
+
+    public void createNewAccount(AccountsModel accountsModel);
+    public void updateAccount(AccountsModel accountsModel);
+    public void getAccountById(int id);
+    public void deleteAccount(int id);
+    public List<AccountsModel> listofAccounts();
+    public List<AccountsModel> deleteAccounts(List<Integer> id);
 }
---
+++ src/main/java/com/athena/accounts/dao/AccountsDAOImp.java
@@ -0,0 +1,31 @@
+package com.athena.accounts.dao;
+
+import com.athena.accounts.model.AccountsModel;
+
+import java.util.List;
+
+public class AccountsDAOImp implements AccountsDAO{
+    public void createNewAccount(AccountsModel accountsModel) {
+
+    }
+
+    public void updateAccount(AccountsModel accountsModel) {
+
+    }
+
+    public void getAccountById(int id) {
+
+    }
+
+    public void deleteAccount(int id) {
+
+    }
+
+    public List<AccountsModel> listofAccounts() {
+        return null;
+    }
+
+    public List<AccountsModel> deleteAccounts(List<Integer> id) {
+        return null;
+    }
+}
