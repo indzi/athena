3638876b0b9cc4e111c3840b19e81784e74c07c4
Indranil Sinha <indzi@programmer.net>
2018-05-26 15:45:07 +0430

    added Repository and session

--- src/main/java/com/athena/accounts/dao/AccountsDAOImp.java
+++ src/main/java/com/athena/accounts/dao/AccountsDAOImp.java
@@ -1,10 +1,17 @@
 package com.athena.accounts.dao;
 
 import com.athena.accounts.model.AccountsModel;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class AccountsDAOImp implements AccountsDAO{
+    private SessionFactory sessionFactory;
+    public void setSessionFactory(SessionFactory sf) {
+        this.sessionFactory = sf;
+    }
     public void createNewAccount(AccountsModel accountsModel) {
 
     }
